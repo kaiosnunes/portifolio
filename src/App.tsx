d77c57611d0180d14b2385b7df957f1d8e26f3e1
Kaio Santos Nunes <kaioventuriniguitar@gmail.com>
2026-02-23 12:22:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+	return (
+		<div className="min-h-screen bg-black flex flex-col items-center justify-center gap-6">
+			<div className="w-32 h-32 bg-gradient-to-r from-purple-500 to-pink-500 rounded-2xl shadow-2xl animate-pulse"></div>
+			<p className="text-white text-2xl font-semibold tracking-wide">
+				Portifólio com React + Vite e Tailwind CSS 🚀
+			</p>
+			<button
+				className="px-6 py-2 bg-white text-black font-bold rounded-lg hover:bg-gray-300 transition"
+				type="button"
+			>
+				Clique aqui
+			</button>
+			q
+		</div>
+	);
+}
+
+export default App;
